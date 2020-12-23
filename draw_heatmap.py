7efb65888fa29f32b24d71297e9d813ab7445357
snljty <snljty@gmail.com>
2020-12-24 00:05:29 +0800

    uncomment something useful

--- draw_heatmap.py
+++ draw_heatmap.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 a = np.loadtxt('Metropolis_Monte_Carlo_result.txt')
 fig, ax = plt.subplots()
 ax.imshow(a, plt.cm.hot)
-# ax.set_xticks(list())
-# ax.set_yticks(list())
+ax.set_xticks(list())
+ax.set_yticks(list())
 fig.savefig('Metropolis_Monte_Carlo_result.png')
 plt.show()
 
